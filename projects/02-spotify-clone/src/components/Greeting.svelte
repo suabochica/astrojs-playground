71412f772df56692693ad6675ea3830fbbd5d537
Sergio L. Benítez D <sergio.kun21@gmail.com>
2024-02-28 08:43:28 -0500

    astro: add greetings svelte component.

---
+++ projects/02-spotify-clone/src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greeting = "";
+
+  if (currentHour < 12) {
+    greeting = "Buenos días dormilón";
+  } else if (currentHour < 18) {
+    greeting = "Buenas tardes hermoso";
+  } else {
+    greeting = "Buenas noches guapo";
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
