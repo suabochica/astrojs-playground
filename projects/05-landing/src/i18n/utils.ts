3063e134464a45f2e8d0ede18ded6c3aba07bd42
Sergio L. Benítez D <sergio.kun21@gmail.com>
2024-04-06 13:28:14 -0500

    astro: add get route from url util.

--- projects/05-landing/src/i18n/utils.ts
+++ projects/05-landing/src/i18n/utils.ts
@@ -10,6 +10,41 @@ export function getLangFromUrl(url: URL) {
   return defaultLanguage
 }
 
+export function getRouteFromUrl(url: URL): string | undefined {
+  const pathname = new URL(url).pathname
+  const parts = pathname?.split('/')
+  const path = parts.pop()
+
+  if (path === undefined) {
+    return undefined
+  }
+
+  const currentLanguage = getLangFromUrl(url)
+
+  if (defaultLanguage === currentLanguage) {
+    const route = Object.values(ROUTES)[0]
+
+    return route[path as keyof typeof route] !== undefined
+      ? route[path as keyof typeof route]
+      : path
+  }
+
+  const getKeyByValue = (
+    obj: Record<string, string>,
+    value: string
+  ): string | undefined => {
+    return Object.keys(obj).find((key) => obj[key] === value)
+  }
+  
+  const reversedKey = getKeyByValue(ROUTES[currentLanguage], path);
+
+  if (reversedKey === undefined) {
+    return reversedKey
+  }
+
+  return undefined
+}
+
 export function useTranslations(language: keyof typeof NAV_ITEMS) {
   return function translate(key: keyof (typeof NAV_ITEMS)[typeof defaultLanguage]) { 
     return NAV_ITEMS[language][key] || NAV_ITEMS[defaultLanguage][key]
