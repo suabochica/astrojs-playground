05e52527824abc1a76131ebbd502ae7dd20e5353
Sergio L. Benítez D <sergio.kun21@gmail.com>
2024-04-08 11:45:46 -0500

    astro: add english as third language.

--- projects/05-landing/src/i18n/index.ts
+++ projects/05-landing/src/i18n/index.ts
@@ -1,18 +1,20 @@
 import catalan from './ca.json'
+import english from './en.json'
 import spanish from './es.json'
 
 const LANGUAGES = {
   CATALAN: 'ca',
+  ENGLISH: 'en',
   SPANISH: 'es'
 }
 
 export const getI18N = ({ currentLocale = 'es' }: { currentLocale: string | undefined }) => {
   if (currentLocale === LANGUAGES.CATALAN) {
-    return catalan
+    return {...spanish, ...catalan}
   }
 
-  if (currentLocale === LANGUAGES.SPANISH) {
-    return spanish
+  if (currentLocale === LANGUAGES.ENGLISH) {
+    return {...spanish, ...english}
   }
 
   return spanish
