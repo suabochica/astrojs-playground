c429ea0458cae9c0659ffeb0e924e9e1fe62637b
Sergio L. Benítez D <sergio.kun21@gmail.com>
2024-09-26 09:44:06 -0500

    astro: manage varenvs, render according values of them and consume them from server or client.

--- projects/06-server-islands/astro.config.mjs
+++ projects/06-server-islands/astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({context: 'server', access: 'public'}),
+      SCORE_API_ENDPOINT: envField.string({context: 'client', access: 'public'})
+    }
+  }
 });
