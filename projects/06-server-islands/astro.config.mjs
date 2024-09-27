f490e75d3be5fb5d206decb8d6f1dce94c54c375
Sergio L. Benítez D. <sergio.kun21@gmail.com>
2024-09-27 14:21:48 -0500

    astro: add book score component as server island.

--- projects/06-server-islands/astro.config.mjs
+++ projects/06-server-islands/astro.config.mjs
@@ -4,11 +4,12 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind()],
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({context: 'server', access: 'public'}),
-      SCORE_API_ENDPOINT: envField.string({context: 'client', access: 'public'})
+      SCORE_API_ENDPOINT: envField.string({context: 'server', access: 'public'})
     }
   }
 });
