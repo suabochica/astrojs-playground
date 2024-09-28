71fb02f921d5cea34c4cbe1a3d65a4f3a5a59cd7
Sergio L. Benítez D <sergio.kun21@gmail.com>
2024-09-28 18:55:22 -0500

    astro: add vercel adapter to get IP from locations.

--- projects/06-server-islands/astro.config.mjs
+++ projects/06-server-islands/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,7 @@ export default defineConfig({
       SHOW_BUY_BUTTON: envField.boolean({context: 'server', access: 'public'}),
       SCORE_API_ENDPOINT: envField.string({context: 'server', access: 'public'})
     }
-  }
+  },
+
+  adapter: vercel()
 });
