b116805e7e74acc92fbd5dd1ab0d6db63c4b7a03
Sergio L. Benítez D <sergio.kun21@gmail.com>
2024-12-10 10:27:13 -0500

    astro: add new project with new images and sponsors.

---
+++ projects/08-evening/src/env.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../.astro/types.d.ts" />
+
+interface Window {
+	getThemePreference(): "dark" | "light"
+}
---
+++ projects/08-evening/tailwind.config.mjs
@@ -0,0 +1,26 @@
+import animations from "@midudev/tailwind-animations"
+
+/** @type {import('tailwindcss').Config} */
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				atomic: ["Atomic", "cursive"],
+			},
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				twitch: "var(--color-twitch)",
+				ice: "var(--color-twitch-ice)",
+			}
+		},
+	},
+	plugins: [
+		animations,
+		function ({ addVariant }) {
+			addVariant("any-hover, @media (any-hover:hover) {&:hover}")
+		},
+	],
+}
