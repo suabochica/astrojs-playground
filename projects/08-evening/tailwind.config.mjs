693641ea721fd4d14045ef55673fe9e8e2aeac7b
Sergio L. Benítez D. <sergio.kun21@gmail.com>
2024-12-12 09:38:51 -0500

    astro: add Typography component and use it on Sponsors component.

--- projects/08-evening/tailwind.config.mjs
+++ projects/08-evening/tailwind.config.mjs
@@ -1,26 +1,27 @@
-import animations from "@midudev/tailwind-animations"
+import animations from "@midudev/tailwind-animations";
 
 /** @type {import('tailwindcss').Config} */
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				atomic: ["Atomic", "cursive"],
-			},
-			colors: {
-				primary: "var(--color-primary)",
-				secondary: "var(--color-secondary)",
-				twitch: "var(--color-twitch)",
-				ice: "var(--color-twitch-ice)",
-			}
-		},
-	},
-	plugins: [
-		animations,
-		function ({ addVariant }) {
-			addVariant("any-hover, @media (any-hover:hover) {&:hover}")
-		},
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        atomic: ["Atomic", "cursive"],
+      },
+      colors: {
+        accent: "var(--color-accent)",
+        ice: "var(--color-twitch-ice)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        twitch: "var(--color-twitch)",
+      },
+    },
+  },
+  plugins: [
+    animations,
+    function ({ addVariant }) {
+      addVariant("any-hover, @media (any-hover:hover) {&:hover}");
+    },
+  ],
+};
